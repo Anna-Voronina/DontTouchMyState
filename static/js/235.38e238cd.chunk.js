4c934cb4414ce40a0d00c9630a157c4657709322
Anna-Voronina <127735848+Anna-Voronina@users.noreply.github.com>
2023-07-19 12:50:44 +0000

    Deploying to gh-pages from @ Anna-Voronina/DontTouchMyState@d898c89b2f5c65885a0c29115aaee15b3e6142a4 🚀

--- static/js/235.38e238cd.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[235],{7235:function(e,t,r){r.r(t);var a=r(3329);t.default=function(){return(0,a.jsx)("div",{children:"HomePage"})}}}]);
-//# sourceMappingURL=235.38e238cd.chunk.js.map
---
+++ static/js/815.09885356.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[815],{2815:function(e,t,r){r.r(t),r.d(t,{default:function(){return c}});r(2791);var n=r(3329),u=function(){return(0,n.jsx)("div",{children:"RegisterForm"})},c=function(){return(0,n.jsx)(u,{})}}}]);
+//# sourceMappingURL=815.09885356.chunk.js.map
